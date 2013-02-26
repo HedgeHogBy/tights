8d96e822d5788c9c3df84f034a52674118784b32
HedgeHogBy <stankevich.e@gmail.com>
2013-02-26 15:10:59 +0300

    main menu controller

--- js/tightsApp.js
+++ js/tightsApp.js
@@ -9,6 +9,72 @@ App.config(function ($routeProvider, $locationProvider) {
         otherwise({redirectTo : "/all-tights"});
 });
 
+App.factory('Categories', function(){
+    return {
+        0: {
+            id: 1,
+            name:'Lycra 10-20 den'
+        },
+        1: {
+            id: 2,
+            name:'Lycra 30-40 den'
+        },
+        2: {
+            id: 3,
+            name:'Microfibra 50-80 den'
+        },
+        3: {
+            id: 4,
+            name:'Coprenti 100-200 den'
+        },
+        4: {
+            id: 5,
+            name:'Modellanti'
+        },
+        5: {
+            id: 6,
+            name:'Fashion'
+        },
+        6: {
+            id: 7,
+            name:'Gambaletti'
+        },
+        7: {
+            id: 8,
+            name:'Autoreggenti'
+        },
+        8: {
+            id: 9,
+            name:'Calzini'
+        },
+        9: {
+            id: 10,
+            name:'Leggings'
+        }
+    };
+});
+
+App.factory('Collections', function(){
+    return {
+        0: {
+            id: 1,
+            name:'BASIC LINE',
+            categories: [1,2,3,5,8]
+        },
+        1: {
+            id: 2,
+            name:'CITY LINE',
+            categories: [1,2,3,4,5,6,7,8,9,10]
+        },
+        2: {
+            id: 3,
+            name:'CITY LINE MEN',
+            categories: [9]
+        }
+    };
+
+});
+
 App.controller('AppCtrl', function AppCtrl($scope) {
 
 });
@@ -18,40 +84,23 @@ App.controller('TightsDetailCtrl', function TightsDetailCtrl(Tights) {
 });
 
 
-App.controller('CollectionsMenuCtrl', function CollectionsMenuCtrl($scope) {
-    $scope.collections = [
-        {
-            name: 'BASIC LINE',
-            categories: [
-                {name:'Lycra 10-20 den'},
-                {name:'Lycra 30-40 den'},
-                {name:'Microfibra 50-80 den'},
-                {name:'Modellanti'},
-                {name:'Autoreggenti'}
-            ]
-        },
-        {
-            name: 'CITY LINE',
-            categories: [
-                {name:'Lycra 10-20 den'},
-                {name:'Lycra 30-40 den'},
-                {name:'Microfibra 50-80 den'},
-                {name:'Coprenti 100-200 den'},
-                {name:'Modellanti'},
-                {name:'Fashion'},
-                {name:'Gambaletti'},
-                {name:'Autoreggenti'},
-                {name:'Calzini'},
-                {name:'Leggings'}
-            ]
-        },
-        {
-            name: 'CITY LINE MEN',
-            categories: [
-                {name:'Leggings'}
-            ]
-        }
-    ];
+App.controller('CollectionsMenuCtrl', function CollectionsMenuCtrl($scope, Collections, Categories) {
+    var cat;
+    $scope.mainMenuItems = [];
+
+    _.each(Collections, function(collection, key){
+        collection.categories = _.map(collection.categories, function(category){
+            cat = _.find(Categories, function(num){return num.id === category;});
+            if(cat){
+                category = {name: cat.name};
+            } else {
+                category = {name: 'Category'+category};
+            }
+            return category;
+        });
+
+        $scope.mainMenuItems[key] = collection;
+    });
 });
 
 
